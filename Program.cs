fe2d2d0e7362e1349c52f8dea7bd533a89ba67ee
Thomas Moffet <pantherman1996@gmail.com>
2021-04-03 07:25:08 +1100

    Simple project that just bootstraps the existing packages.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BlendoBot.Live {
+	public class Program {
+		public static void Main(string[] args) {
+			var blendobot = new BlendoBot("config.cfg");
+			blendobot.Start(args).ConfigureAwait(false).GetAwaiter().GetResult();
+		}
+	}
+}
